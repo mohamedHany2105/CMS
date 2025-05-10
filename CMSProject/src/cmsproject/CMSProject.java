e5f98a15e3bb4b2ee6e3b0443c27cdb993821107
mohamedHany2105 <187531541+mohamedHany2105@users.noreply.github.com>
2025-05-10 19:17:43 +0300

    Add files via upload
    first commit

---
+++ CMSProject/src/cmsproject/CMSProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cmsproject;
+
+/**
+ *
+ * @author hanys
+ */
+public class CMSProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
